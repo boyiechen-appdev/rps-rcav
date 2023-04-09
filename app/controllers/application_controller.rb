357de3088b3e28ff6136644a96debe056457b53c
boyiechen <boyie@uchicago.edu>
2023-04-09 02:11:28 +0000

    learn route and template

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,10 @@ class ApplicationController < ActionController::Base
 
   # Add your actions below this line
   # ================================
-
+  def play_rock
+    # redirect_to("https://wiki.ubuntu-tw.org/index.php?title=首頁")
+    # render({ :plain => "Hello, world!" })
+    # render({ :html => "<h1>Hello, world!</h1>".html_safe })
+    render({ :template => "game_templates/user_rock.html.erb" })
+  end
 end
